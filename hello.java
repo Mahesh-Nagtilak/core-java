ee836d6ce4bf2a602a387bd0a97790e7f2221133
Mahesh <maheshnagtilak97@gmail.com>
2022-01-21 20:58:47 +0530

    madified hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
 public static void main(){
 
 System.out.println("hello from git local repository");
-System.out.println("hello from mahesh");
+System.out.println("hello from mahesh nagesh");
 
 }
 
